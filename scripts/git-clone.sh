953c7c6ec7b05ec80cd8fc9cdbab79f7f948e7cd
Liviu Ionescu <ilg@livius.net>
2019-07-06 19:34:49 +0300

    git-clone.sh: use ${HOME}

--- scripts/git-clone.sh
+++ scripts/git-clone.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-rm -rf ~/Downloads/qemu-build.git
-git clone --recurse-submodules https://github.com/gnu-mcu-eclipse/qemu-build.git ~/Downloads/qemu-build.git
+rm -rf "${HOME}/Downloads/qemu-build.git"
+git clone --recurse-submodules https://github.com/gnu-mcu-eclipse/qemu-build.git "${HOME}/Downloads/qemu-build.git"
